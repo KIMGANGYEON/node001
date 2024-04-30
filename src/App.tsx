6dd9e12cf15e12e19fe9d372eb8ab4fd18447af6
KIMGANGYEON <rkddus3177@naver.com>
2024-05-01 00:03:52 +0900

    ..

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/api")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
+  return <h1>hello</h1>;
+}
+
+export default App;
